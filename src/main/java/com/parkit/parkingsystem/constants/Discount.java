f25f24d6b1891fdcc3cfd81c9ec6e0c265c1da84
Perrine Dassonville <perrine.dassonville@outlook.fr>
2024-05-14 16:08:27 +0200

    ajout de constantes Discount

---
+++ src/main/java/com/parkit/parkingsystem/constants/Discount.java
@@ -0,0 +1,8 @@
+package com.parkit.parkingsystem.constants;
+
+public class Discount {
+
+    public static final double MAX_HOURS_FREE_PARKING = 0.5;
+    public static final double RECURRING_USER_DISCOUNT_PERCENTAGE = 0.95;
+    public static final byte MINIMUM_VISITS_FOR_DISCOUNT = 1;
+}
