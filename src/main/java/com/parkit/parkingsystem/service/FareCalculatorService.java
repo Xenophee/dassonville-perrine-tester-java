3afeadb293d12dbfbf8b5775001a36281df9f45c
Perrine Dassonville <perrine.dassonville@outlook.fr>
2024-05-14 16:11:17 +0200

    mise à jour de la classe avec la fonctionnalité des 5% et factorisation du code

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -3,46 +3,49 @@ package com.parkit.parkingsystem.service;
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
+import static com.parkit.parkingsystem.constants.Discount.MAX_HOURS_FREE_PARKING;
+import static com.parkit.parkingsystem.constants.Discount.RECURRING_USER_DISCOUNT_PERCENTAGE;
+import static com.parkit.parkingsystem.util.NumbersUtil.roundDecimals;
+
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
-        if(ticket.getOutTime() == null || ticket.getOutTime().isBefore(ticket.getInTime())) {
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
+    public void calculateFare(Ticket ticket) {
+        calculateFare(ticket, false);
+    }
+
+    public void calculateFare(Ticket ticket, boolean discount) {
+        if (ticket.getOutTime() == null || ticket.getOutTime().isBefore(ticket.getInTime())) {
+            throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
         }
 
         LocalDateTime inTime = ticket.getInTime();
         LocalDateTime outTime = ticket.getOutTime();
 
-        Duration nanoDuration = Duration.between(inTime, outTime);
-        double duration = (double) nanoDuration.toMillis() / (60 * 60 * 1000);
-//        double duration = duration2.toMillis() / (60 * 60 * 1000.0);
+        long durationInMinutes = Duration.between(inTime, outTime).toMinutes();
+        double durationInHours = roundDecimals(durationInMinutes / 60.0, 2);
 
-        duration = BigDecimal.valueOf(duration)
-                .setScale(3, RoundingMode.HALF_UP)
-                .doubleValue();
+        if (durationInHours <= MAX_HOURS_FREE_PARKING) return;
 
-        if (duration <= 0.5) {
-            ticket.setPrice(0);
-        } else {
-            switch (ticket.getParkingSpot().getParkingType()) {
-                case CAR: {
-                    ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
-                    break;
-                }
-                case BIKE: {
-                    ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
-                    break;
-                }
-                default:
-                    throw new IllegalArgumentException("Unknown Parking Type");
+        double price;
+
+        switch (ticket.getParkingSpot().getParkingType()) {
+            case CAR: {
+                price = durationInHours * Fare.CAR_RATE_PER_HOUR;
+                break;
             }
+            case BIKE: {
+                price =  durationInHours * Fare.BIKE_RATE_PER_HOUR;
+                break;
+            }
+            default:
+                throw new IllegalArgumentException("Unknown Parking Type");
         }
 
+        if (discount) price *= RECURRING_USER_DISCOUNT_PERCENTAGE;
 
+        ticket.setPrice(roundDecimals(price, 2));
     }
 }
