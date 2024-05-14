7da171529881aadf6a235f80459bf6eedd258828
Perrine Dassonville <perrine.dassonville@outlook.fr>
2024-05-14 16:20:11 +0200

    correctif de la visibilité d'une méthode

--- src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
@@ -32,7 +32,7 @@ public class ParkingServiceTest {
     private static TicketDAO ticketDAO;
 
     @BeforeEach
-    private void setUpPerTest() {
+    public void setUpPerTest() {
         try {
             when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
 
